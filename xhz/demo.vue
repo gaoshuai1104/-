a719754e126cf8b11f18dd38d489eebad7820fc4
gaoshuai1104 <1217188762@qq.com>
2019-09-05 15:05:35 +0800

    测试Model

--- xhz/demo.vue
+++ xhz/demo.vue
@@ -4,17 +4,17 @@
     </div>
 </template>
 <script>
-import '../xhz/jquery-ui.js'
-import '../xhz/BIMWINNER.viewer.min.js'
-import '../xhz/bootstrap-treeview.min.js'
-import '../xhz/jquery.min.js'
-import '../xhz/jszip.min.js'
-import '../xhz/spectrum.js'
-import '../xhz/three.min.js'
+// import '../xhz/jquery-ui.js'
+// import '../xhz/BIMWINNER.viewer.min.js'
+// import '../xhz/bootstrap-treeview.min.js'
+// import '../xhz/jquery.min.js'
+// import '../xhz/jszip.min.js'
+// import '../xhz/spectrum.js'
+// import '../xhz/three.min.js'
 export default {
     
 }
 </script>
 <style>
-    @import '../xhz/BIMWINNER.viewer.min.css'
+    /* @import '../xhz/BIMWINNER.viewer.min.css' */
 </style>
